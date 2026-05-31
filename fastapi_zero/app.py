7445efc70e31c8c54f3661c2f1e9124d57d7dcac
João Pedro <norbas@pop-os.localdomain>
2026-05-31 01:12:37 -0300

    Aula 03

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,10 +1,49 @@
-from fastapi import FastAPI
+from http import HTTPStatus
 
-from fastapi_zero.schemas import Message
+from fastapi import FastAPI, HTTPException
+
+from fastapi_zero.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserPublicSchema,
+    UserSchema,
+)
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', response_model=Message)
 def read_root():
     return {'message': 'Hello World'}
+
+
+@app.post(
+    '/users/', status_code=HTTPStatus.CREATED, response_model=UserPublicSchema
+)
+def create_user(user: UserSchema):
+
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublicSchema)
+def update_user(user_id: int, user: UserSchema):
+    if not 0 < user_id < len(database):
+        raise H
+    
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,53 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
-from fastapi_zero.app import app
 
-
-def test_read_root_return_hello_world_and_ok():
-    client = TestClient(app)
-
+def test_read_root_return_hello_world_and_ok(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'user',
+            'email': '123@teste.com',
+            'password': 'senhafoda',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'user',
+        'email': '123@teste.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [{'id': 1, 'username': 'user', 'email': '123@teste.com'}]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': 'senha',
+            'username': 'username',
+            'email': '123@teste.com',
+            'id': 1,
+        },
+    )
+
+    assert response.json() == {
+        'username': 'username',
+        'email': '123@teste.com',
+        'id': 1,
+    }
