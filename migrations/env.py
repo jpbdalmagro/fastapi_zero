0d48c9eaa45cc5ac1f3d8087ca35a3dd18b0a019
jpbdalmagro <jpbdalmagro@gmail.com>
2026-07-23 16:26:16 -0300

    final da aula 04 e exercicíos

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,12 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from fastapi_zero.models import table_registry
+from fastapi_zero.settings import Settings
+
+config = context.config
+config.set_main_option("sqlalchemy.url", Settings().DATABASE_URL)
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = table_registry.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
